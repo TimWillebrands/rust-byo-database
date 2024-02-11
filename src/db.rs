a117ac823778053aff9c572687ee997136f2d117
Tim Willebrands <mail@timwillebrands.nl>
2024-02-11 13:48:51 +0100

    headers are working

--- src/db.rs
+++ src/db.rs
@@ -1,32 +1,96 @@
-const HEADER:u32 = 4;
-const BTREE_PAGE_SIZE:u32 = 4096;
-const BTREE_MAX_KEY_SIZE:u32 = 1000;
-const BTREE_MAX_VAL_SIZE:u32 = 3000;
+const HEADER: u32 = 4;
+const BTREE_PAGE_SIZE: u32 = 4096;
+const BTREE_MAX_KEY_SIZE: u32 = 1000;
+const BTREE_MAX_VAL_SIZE: u32 = 3000;
 
+#[derive(Debug)]
 pub enum BNodeType {
     Node,
     Leaf,
 }
 
 impl BNodeType {
-    pub fn header(&self) -> u8 {
+    pub fn as_u16(&self) -> u16 {
         match *self {
             BNodeType::Node => 1,
             BNodeType::Leaf => 2,
         }
     }
 }
+
+//  | type | nkeys |  pointers  |   offsets  | key-values
+//  |  2B  |   2B  | nkeys * 8B | nkeys * 2B | ...
 pub struct BNode {
-    data: [u8; 5]// can be dumped to the disk
+    data: Vec<u8>, // can be dumped to the disk
 }
 
 pub struct BTree {
     // pointer (a nonzero page number)
-    root: u64, 
+    root: u64,
     // callbacks for managing on-disk pages
     // get func(uint64) BNode // dereference a pointer
     // new func(BNode) uint64 // allocate a new page
     // del func(uint64)       // deallocate a page
+}
+
+impl BNode {
+    pub fn new(typ: BNodeType, keys: u16) -> BNode {
+        let [type_high, type_low] = typ.as_u16().to_le_bytes();
+        let [keys_high, keys_low] = keys.to_le_bytes();
+
+        return BNode {
+            data: vec![type_high, type_low, keys_high, keys_low],
+        };
+    }
+
+    pub fn btype(&self) -> Result<BNodeType, String> {
+        let bytes: [u8; 2] = self.data[0..2]
+            .try_into()
+            .expect("type slice with incorrect length");
+
+        let typ = u16::from_le_bytes(bytes);
+
+        match typ {
+            1 => Ok(BNodeType::Node),
+            2 => Ok(BNodeType::Leaf),
+            _ => Err("Unrecognised node type!".to_string()),
+        }
+    }
+
+    pub fn nkeys(&self) -> u16 {
+        let bytes: [u8; 2] = self.data[2..4]
+            .try_into()
+            .expect("keys slice with incorrect length");
+
+        u16::from_le_bytes(bytes)
+    }
+
+    pub fn set_header(&mut self, typ: BNodeType, keys: u16) {
+        let [type_high, type_low] = typ.as_u16().to_le_bytes();
+        let [keys_high, keys_low] = keys.to_le_bytes();
+
+        self.data[0] = type_high;
+        self.data[1] = type_low;
+        self.data[2] = keys_high;
+        self.data[3] = keys_low;
+    }
+
+    pub fn get_ptr(&self, idx: u16) -> Result<u64, String> {
+        if idx >= self.nkeys() {
+            return Err("Index out of bounds".to_string());
+        }
+        let pos = usize::from((HEADER as u16) + 8 * idx);
+
+        if pos + 8 > self.data.len() {
+            return Err("Index out of data range".to_string());
+        }
+
+        // Safely extract a u64 from the data slice
+        let ptr_bytes = &self.data[pos..pos + 8];
+        let ptr = u64::from_le_bytes(ptr_bytes.try_into().expect("slice with incorrect length"));
+
+        Ok(ptr)
+    }
 }
 
 #[cfg(test)]
@@ -37,13 +101,32 @@ mod tests {
     fn headers_stay_the_same() {
         let node = BNodeType::Node;
         let leaf = BNodeType::Leaf;
-        assert_eq!(node.header(), 1);
-        assert_eq!(leaf.header(), 2);
+        assert_eq!(node.as_u16(), 1);
+        assert_eq!(leaf.as_u16(), 2);
     }
 
     #[test]
     fn check_constraints() {
         let node1max = HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE;
         assert!(node1max <= BTREE_PAGE_SIZE)
+    }
+
+    #[test]
+    fn create_node_and_mutate_headers() {
+        let mut node = BNode::new(BNodeType::Node, 4);
+
+        let btype = node.btype();
+        assert!(matches!(btype, Ok(BNodeType::Node)));
+
+        let keys = node.nkeys();
+        assert_eq!(keys, 4);
+
+        node.set_header(BNodeType::Leaf, 6);
+
+        let btype = node.btype();
+        assert!(matches!(btype, Ok(BNodeType::Leaf)));
+
+        let keys = node.nkeys();
+        assert_eq!(keys, 6);
     }
 }
